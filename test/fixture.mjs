8a848b2aaff547afe552deb87ae18290c3365491
Laurent Christophe <lachrist@vub.ac.be>
2022-09-07 12:16:14 +0200

    test: test modules

---
+++ test/fixture.mjs
@@ -0,0 +1,17 @@
+
+import { join as joinPath } from "node:path";
+import {tmpdir as getTmpdir} from "node:os";
+export {
+  equal as assertEqual,
+  deepEqual as assertDeepEqual,
+  rejects as assertReject,
+  throws as assertThrow,
+} from "node:assert/strict";
+
+const { now } = Date;
+const { random } = Math;
+
+export const getTemporaryPath = () => joinPath(
+  getTmpdir(),
+  `test_turtle_${now().toString(36)}_${random().toString(36).slice(2)}`,
+);
